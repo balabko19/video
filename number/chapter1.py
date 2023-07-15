9b2a986ffaac3bb9738788e80cb075c9ec7122c8
balabko19 <137001662+balabko19@users.noreply.github.com>
2023-07-15 18:57:15 +0300

    Update chapter1.py

--- number/chapter1.py
+++ number/chapter1.py
@@ -17,11 +17,22 @@ class whatfunction(Scene):
             Tex('M').scale(0.45)
             )
         
+        label2 = axx.get_axis_labels(
+            Tex('X').scale(0.45),
+            Tex('Y').scale(0.45)
+            )
+        
         str1 = Text ('N - колличество еды\nМ - колличество килограмм', font_size=18).shift(UR *2 + RIGHT)
-        str1[0].set_color(RED) ###ПЕРЕДЕЛАТЬ
-        str1[16].set_color(RED)
+        str2 = Tex('$M = f(N)$').next_to(axx, UP)
+        str3 = Text ('X - аргумент\nY - зависимая величина', font_size=18).shift(UR *2 + RIGHT)
+        str4 = Tex('$Y = f(X)$').next_to(axx, UP)
 
-        dots = VGroup(*[Dot() for j in range(5)]).arrange(np.around(axx.coords_to_point(0, 0, 0), 0), buff= 0.01)
+        #dots = VGroup(*[Dot() for j in range(5)]).arrange(LEFT, buff = 1)
+        line_graph= axx.plot_line_graph(
+            x_values =[0,2,5],
+            y_values =[0,1,2.5],
+            line_color= RED
+        )
 
 
 
@@ -34,10 +45,14 @@ class whatfunction(Scene):
         self.wait(2)
         self.play(Create(str1))
         self.wait(2)
-        self.play(Create(dots))
+        self.play(Create(str2))
         self.wait(2)
-        ####
-        
+        self.play(Create(line_graph))
+        self.wait(2)
+        self.play(Transform(str1,str3), Transform(label, label2), Transform(str2,str4 ))
+
+
+
 
 
 
